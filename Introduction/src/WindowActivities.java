3b70d5ed481cc348ff401d69e37676eb1979fb73
JackMunn <jackmunn@gmail.com>
2022-12-08 11:30:26 +0000

    Finished section 6

---
+++ Introduction/src/WindowActivities.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowActivities {
+	public static void main (String[] args) {
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		WebDriver website = new ChromeDriver();
+		
+		//viewport settings
+		website.manage().window().maximize();
+		website.get("http://www.google.com"); // this way of going to a website waits until all web components are loaded before continuing
+		website.navigate().to("http://www.bbc.co.uk"); //this way does it, it'll immediately move to the next step
+		website.navigate().back();
+		website.navigate().forward();
+	}
+}
